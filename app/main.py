6e06bb59a7d5a1967f33e099db39b645a045717c
jestenok <112510943+jestenok@users.noreply.github.com>
2023-03-30 23:01:10 +0800

    add system information

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from remote.router import new_app
 
 
 if __name__ == '__main__':
+    print('Starting server')
     init_db()
+    print('DB initialized')
     web.run_app(new_app(),
                 port=8080)
