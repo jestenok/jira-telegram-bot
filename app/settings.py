5fa35089d6f139a94afb9ee61c28df926560b0eb
jestenok <112510943+jestenok@users.noreply.github.com>
2023-03-30 15:19:43 +0800

    remove env parse

--- app/settings.py
+++ app/settings.py
@@ -4,38 +4,25 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.engine.url import URL
 
 import os
-from pathlib import Path
 
 from telegram import Bot
-
-
-with open('.env', 'r') as fh:
-    vars_dict = {}
-    for line in fh:
-        if line.startswith('#') or line == '\n':
-            continue
-        key, val = line.replace('\n', '').split('=', maxsplit=1)
-        vars_dict[key] = val.strip()
-
-os.environ.update(vars_dict)
-# dotenv_path = Path(__file__).parent.joinpath('.env')
-# load_dotenv(dotenv_path)
 
 TOKEN = os.environ.get('TELEGRAM_TOKEN')
 bot = Bot(TOKEN)
 
 JIRA_HOST = os.environ.get('JIRA_HOST')
-JIRA_DOMAINNAME = os.environ.get("JIRA_DOMAINNAME")
-JIRA_AUTH_LINK = f'{JIRA_DOMAINNAME}/secure/ViewProfile.jspa?' \
+
+JIRA_PUBLIC_DOMAINNAME = os.environ.get("JIRA_PUBLIC_DOMAINNAME")
+JIRA_AUTH_LINK = f'{JIRA_PUBLIC_DOMAINNAME}/secure/ViewProfile.jspa?' \
                  f'selectedTab=com.atlassian.pats.pats-plugin:jira-user-personal-access-tokens'
 
 DATABASE = {
     'drivername': 'postgresql',
     'host': os.environ.get('DB_HOST'),
-    'port': '5432',
-    'database': 'server',
+    'port': os.environ.get('DB_PORT'),
+    'database': os.environ.get('DB_NAME'),
     'username': os.environ.get('DB_USERNAME'),
-    'password': os.environ.get('PASSWORD')
+    'password': os.environ.get('DB_PASSWORD')
 }
 
 engine = create_engine(URL.create(**DATABASE),
