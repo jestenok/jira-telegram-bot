c251d15170107ecbb5b0b088ee51ebf2e7c1223c
jestenok <112510943+jestenok@users.noreply.github.com>
2023-03-30 15:19:30 +0800

    change port number

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
-# from app.settings import init_db
+from app.settings import init_db
 from aiohttp import web
 from remote.router import new_app
 
 
 if __name__ == '__main__':
-    # init_db()
+    init_db()
     web.run_app(new_app(),
-                # host='localhost',
-                port=80)
+                port=8080)
