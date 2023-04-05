f89511fde14332a7562d8e5cff3501d5a0e5e58f
jestenok <112510943+jestenok@users.noreply.github.com>
2023-04-05 15:39:09 +0300

    change log format

--- app/remote/middleware.py
+++ app/remote/middleware.py
@@ -1,12 +1,15 @@
 import logging
 from aiohttp.web import middleware
+import json
 
 
 @middleware
 async def log_middleware(request, handler):
     data = await request.json()
 
-    info = f'''method: {request.method}, url: {request.url}, json: {data}'''
+    j = json.dumps(data, indent=4, sort_keys=True)
+
+    info = f'''method: {request.method}, url: {request.url}, json: {j}'''
 
     logging.info(info)
 
