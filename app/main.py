f445da2eee625f530c534efee89175ec09a738f5
jestenok <112510943+jestenok@users.noreply.github.com>
2023-04-07 13:34:03 +0300

    test

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
-from app import init_db, TOKEN
-from aiohttp import web
-from remote.router import create_routes
-from remote.middleware import log_middleware
-import logging
-
+# from app import init_db, TOKEN
+# from aiohttp import web
+# from remote.router import create_routes
+# from remote.middleware import log_middleware
+# import logging
+#
 
 if __name__ == '__main__':
-    logging.info('Starting server')
-
-    init_db()
-    logging.info('DB initialized')
-
-    app = web.Application()
-    app.add_routes(create_routes(TOKEN))
-    app.middlewares.append(log_middleware)
-
-    web.run_app(app,
-                access_log=None,
-                port=8080)
+    print('Starting server')
+    # logging.info('Starting server')
+    #
+    # init_db()
+    # logging.info('DB initialized')
+    #
+    # app = web.Application()
+    # app.add_routes(create_routes(TOKEN))
+    # app.middlewares.append(log_middleware)
+    #
+    # web.run_app(app,
+    #             access_log=None,
+    #             port=8080)
